88b5bc677578ffa055ce6afd94c82a3ffb87e5ea
dptindugan <dptindugan@gmail.com>
2021-03-20 19:54:33 +0800

    added en passant move

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,11 +15,21 @@ let STARTING_SQR = null;
 let CAPTURED_PIECES = [];
 let PLAYER_TURN = 'white';
 let isEnPassant = false;
+let enPassantTarget = null;
 
 function renderPiece(rowIndex, colIndex) {
+
+  /* initial definition array  
+    0 -> represents black special pieces starting row
+    1 -> represents black pawns starting row
+    6 -> represents white pawns starting row
+    7 -> represents white special pieces starting row
+  */
+
   const initialRowPositions = [0, 1, 6, 7]
   if(initialRowPositions.includes(rowIndex)){
 
+  // create a piece element, this will be move between squares
   const piece = document.createElement('span');
     piece.classList.add('piece')
     const blackPieces = {
@@ -44,6 +54,7 @@ function renderPiece(rowIndex, colIndex) {
       7: whites.rook, 
     }
 
+    // check which piece will go to certain squares
     if(rowIndex === 0) {
       piece.innerHTML = blackPieces[colIndex]
       piece.setAttribute('data-color', 'black')
@@ -76,33 +87,37 @@ function renderPiece(rowIndex, colIndex) {
   return null
 }
 
-rows.forEach((row, rowIndex)=> {
-  column.forEach((col, colIndex) => {
 
-    const piece = renderPiece(rowIndex, colIndex);
-    const color = () => {
-      if(rowIndex % 2 === 0) {
-        return colIndex % 2 === 0 ? 'white' : 'black';
+function initGame() {
+  rows.forEach((row, rowIndex)=> {
+    column.forEach((col, colIndex) => {
+      
+      const piece = renderPiece(rowIndex, colIndex);
+      const color = () => {
+        if(rowIndex % 2 === 0) {
+          return colIndex % 2 === 0 ? 'white' : 'black';
+        }
+        return colIndex % 2 === 0 ? 'black' : 'white';
       }
-      return colIndex % 2 === 0 ? 'black' : 'white';
-    }
-    const square = document.createElement('div');
-    
-    square.setAttribute('data-index', `${row}${rowIndex}_${col}${colIndex}`);
-    square.classList.add('cell');
-    square.classList.add(color());
-    if(piece) {
-
-      const pieceColor = piece.getAttribute('data-color')
-      square.appendChild(piece);
-      if(pieceColor === PLAYER_TURN) {
-        square.addEventListener('click', selectPiece)
+      const square = document.createElement('div');
+      
+      square.setAttribute('data-index', `${row}${rowIndex}_${col}${colIndex}`);
+      square.classList.add('cell');
+      square.classList.add(color());
+      if(piece) {
+        
+        const pieceColor = piece.getAttribute('data-color')
+        square.appendChild(piece);
+        if(pieceColor === PLAYER_TURN) {
+          square.addEventListener('click', selectPiece)
+        }
       }
-    }
-    board.appendChild(square);
-  })
-});
+      board.appendChild(square);
+    })
+  });
+}
 
+// function to help for checkubg pieceType and check possible moves to delete
 function possibleMoves(piece, position) {
   const pieceType = piece.getAttribute("data-piece");
   const pieceColor = piece.getAttribute("data-color");
@@ -166,16 +181,51 @@ function movePiece(e) {
   SELECTED_PIECE.style.top = `${e.pageY - 25}px`
 }
 
+function wouldEnPassant(targetSquare, startingRow) {
+  const position = targetSquare.getAttribute('data-index');
+  const row = position.split('_')[0];
+  const col = position.split('_')[1];
+
+  const leftSquare = document.querySelector(`[data-index=${row}_c${parseInt(col[1]) - 1}]`);
+  const rightSquare = document.querySelector(`[data-index=${row}_c${parseInt(col[1]) + 1}]`);
+  if(startingRow == 'r1' || startingRow == 'r6') {
+    // check if there's an adjacent piece to target square;
+    if(leftSquare?.childNodes[0]) {
+      const pieceOnLeft = leftSquare.childNodes[0];
+      const pieceType = pieceOnLeft.getAttribute('data-piece');
+      const pieceOnLeftColor = pieceOnLeft.getAttribute('data-color');
+      const pawnPiece = [whites.pawn, black.pawn]
+
+      if(pawnPiece.includes(pieceType) && pieceOnLeftColor !== PLAYER_TURN) {
+        isEnPassant = true;
+      }
+    }
+
+    if(rightSquare?.childNodes[0]) {
+      const pieceOnRight = rightSquare.childNodes[0];
+      const pieceType = pieceOnRight.getAttribute('data-piece');
+      const pieceOnRightColor = pieceOnRight.getAttribute('data-color');
+      const pawnPiece = [whites.pawn, black.pawn]
+
+      if(pawnPiece.includes(pieceType) && pieceOnRightColor !== PLAYER_TURN) {
+        isEnPassant = true;
+      }
+    }
+  }
+}
+
 function dropPiece(e) {
   const targetSquare = e.currentTarget;
 
   capturePiece(targetSquare, SELECTED_PIECE);
   const pieceData = SELECTED_PIECE.getAttribute('data-piece');
+  const startingPostiion = SELECTED_PIECE.getAttribute('id');
+  const startingRow = startingPostiion.split('_')[0];
 
   if(pieceData == whites.pawn || pieceData == black.pawn) {
     promotePawn(targetSquare, SELECTED_PIECE);
+    wouldEnPassant(targetSquare, startingRow)
   }
-  
 
   targetSquare.appendChild(SELECTED_PIECE);
   const position = targetSquare.getAttribute('data-index');
@@ -208,6 +258,14 @@ function dropPiece(e) {
 function capturePiece(targetSquare, newPiece) {
   const piece = targetSquare.childNodes[0];
 
+  if(isEnPassant && enPassantTarget) {
+    CAPTURED_PIECES.push(piece.getAttribute('data-piece'));
+    enPassantTarget.removeChild(enPassantTarget.childNodes[0]);
+
+    isEnPassant = false;
+    enPassantTarget = null;
+  }
+
   const attribute = targetSquare.childNodes[0].getAttribute('class');
   if(attribute == 'piece') {
     CAPTURED_PIECES.push(piece.getAttribute('data-piece'))
@@ -231,7 +289,10 @@ function switchTurn() {
 }
 
 function PawnPossibleMoves(row, col) {
-  const direction = (x, num = row[1],) => (PLAYER_TURN == 'white' ? parseInt(num) - x : parseInt(num) + x);
+  const direction = (x, num = row[1]) => (PLAYER_TURN == 'white' ? parseInt(num) - x : parseInt(num) + x);
+  
+  // check if pawn is from starting row
+  // if from starting row, add ability for pawn to moves 2 squares
   if(row == 'r6' || row == 'r1') {
     const leftDiagSqr = getTargetSquare(direction(1), col[1] - 1);
     const rightDiagSqr = getTargetSquare(direction(1), parseInt(col[1]) + 1);
@@ -258,13 +319,17 @@ function PawnPossibleMoves(row, col) {
 
     const leftDiagSqr = getTargetSquare(direction(1), col[1] - 1);
     const rightDiagSqr = getTargetSquare(direction(1), parseInt(col[1]) + 1);
+    const left = getTargetSquare(row[1], col[1] - 1);
+    const right = getTargetSquare(row[1], parseInt(col[1]) + 1);
 
-    if(leftDiagSqr?.childNodes[0]) {
+    if(leftDiagSqr?.childNodes[0] || (left?.childNodes[0] && isEnPassant) ) {
       highlightPossibleSquare(leftDiagSqr)
+      enPassantTarget = left;
     }
     
-    if(rightDiagSqr?.childNodes[0]) {
-      highlightPossibleSquare(rightDiagSqr)
+    if(rightDiagSqr?.childNodes[0] || (right?.childNodes[0] && isEnPassant)) {
+      highlightPossibleSquare(rightDiagSqr);
+      enPassantTarget = right;
     }
 
     if(!square.childNodes[0]) {
@@ -273,6 +338,8 @@ function PawnPossibleMoves(row, col) {
   }
 }
 
+// function helper to highlight possible moves and 
+// add click listener to drop piece on that square
 function highlightPossibleSquare(square) {
   const highlight = document.createElement('div');
   highlight.classList.add('highlight');
@@ -281,6 +348,7 @@ function highlightPossibleSquare(square) {
   square.addEventListener('click', dropPiece);
 }
 
+// NOTE: break -> forcibly stops the loop;
 function diagonalMove(row, col, piece, color) {
   let cUpRight = parseInt(col[1]) + 1;
   let cUpLeft = parseInt(col[1]) - 1;
@@ -318,6 +386,7 @@ function diagonalMove(row, col, piece, color) {
         if(upperLeftSqr && wouldCapture(upperLeftSqr)) {
           highlightPossibleSquare(upperLeftSqr)
         }
+        // if piece is blocked by a piece break/stop the loop;
         break;
       }
 
@@ -491,6 +560,8 @@ function knightMove(row, col) {
   if(lowerLeftSqr2 && wouldCapture(lowerLeftSqr2)) highlightPossibleSquare(lowerLeftSqr2);
 }
 
+// checker if a piece can capture a piece 
+// check if piece color of target piece if opposite of player can capture
 function wouldCapture(targetSquare) {
   if(targetSquare.childNodes[0]) {
     return targetSquare.childNodes[0].getAttribute('data-color') !== PLAYER_TURN;
@@ -502,9 +573,10 @@ function getTargetSquare(row, col) {
   return document.querySelector(`[data-index='r${row}_c${col}']`);
 }
 
+// checker if a piece path is blocked by another piece
+// to prevent SELECTED_PIECE from jumping behind pieces
 function isBlocked(color, targetSquare) {
   let targetColor;
-  
   if(targetSquare.childNodes[0]) {
     targetColor = targetSquare.childNodes[0].getAttribute('data-color');
   }
@@ -512,6 +584,7 @@ function isBlocked(color, targetSquare) {
   if(color == targetColor || targetSquare.childNodes[0]?.getAttribute('class') === 'piece') {
     return true
   }
-
   return false
 }
+
+initGame();
